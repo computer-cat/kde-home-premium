ff1b88aa14f6c2b3a23e1863b3acf8b1cf78e32d
Sylvie00 <taylorkellogg540@gmail.com>
2026-06-14 22:04:48 -0500

    add various packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,21 @@
 
 set -ouex pipefail
 
+#install niri from copr
+dnf5 -y copr enable yalter/niri
+dnf5 install -y sddm niri
+
+#install sunshine copr
+dnf5 -y copr enable lizardbyte/stable
+dnf5 install -y sunshine
+
+#disable copr repos
+dnf5 -y copr disable yalter/niri 
+dnf5 -y copr disable lizardbyte/stable
+
+#install most other things
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf install -y fastfetch steam gamescope mangohud tailscale firefox zoxide erofs-utils
 
 systemctl enable podman.socket
+systemctl enable tailscaled
