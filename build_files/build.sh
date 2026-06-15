696d4ec8cc89adc99ffcd5fda302738fc58dd839
Sylvie00 <taylorkellogg540@gmail.com>
2026-06-14 21:34:39 -0500

    add erofs utils and some gamerslop

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,7 @@
 
 set -ouex pipefail
 
-dnf5 -y copr enable yalter/niri
-dnf5 install -y sddm niri
-
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install -y fastfetch steam tailscale firefox zoxide
+dnf install -y fastfetch steam gamescope mangohud tailscale firefox zoxide erofs-utils
 
 systemctl enable podman.socket
